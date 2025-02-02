55b2c13aa141e2c55350bbf2b767e3b642fcca10
iTzJulians <163297432+iTzJulians@users.noreply.github.com>
2025-02-02 14:05:32 -0500

    burbuja

--- Assets/Bubble.cs
+++ Assets/Bubble.cs
@@ -4,11 +4,20 @@ public class Bubble : MonoBehaviour
 {
     Rigidbody rb;
     public float speed;
+    public int damage;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.linearVelocity = transform.forward * speed;
     }
 
-    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<PlayerMovement>(out PlayerMovement target))
+        {
+            target.GetComponent<Idamageable>().DealDamage(damage);
+        }
+    }
+
+
 }
